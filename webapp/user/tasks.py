088da50680eff561a994fc2fc904ebec5a154935
Styapin Ilya <straigan@pop-os.localdomain>
2023-02-25 01:17:09 +0500

    Add redis and celery

--- webapp/user/tasks.py
+++ webapp/user/tasks.py
@@ -1,20 +1,22 @@
 import smtplib
 
 from webapp.config import MAIL_USERNAME, MAIL_PASSWORD
+from webapp.celery_app import celery
 
-def send_mail(sent_to_email):
+@celery.task
+def send_mail(params_send_email):
     """Отправка письма пользователю с поздравление регистрации его на сайте"""
 
-    sent_from_email = MAIL_USERNAME
+    send_from_email = MAIL_USERNAME
     email_text = 'Hello, thank you for registering on our site.'
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(MAIL_USERNAME, MAIL_PASSWORD)
-        server.sendmail(sent_from_email, sent_to_email, email_text)
+        server.sendmail(send_from_email, params_send_email['send_to_email'], email_text)
         server.close()
 
-        print('Email sent!')
+        print('Email send!')
     except:
         print('Something went wrong...')
